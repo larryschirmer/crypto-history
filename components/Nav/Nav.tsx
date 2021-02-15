57040437fcd30a563181471aeeff69090d572c57
Larry Schirmer <larryschirmer@gmail.com>
2021-02-15 12:09:40 -0500

    feat: toggle settings

--- components/Nav/Nav.tsx
+++ components/Nav/Nav.tsx
@@ -1,16 +1,24 @@
-import React from 'react';
+import React, { FC } from 'react';
+import { useRouter } from 'next/router';
 
 import styles from './Nav.module.scss';
 
 const { nav: navClass, logo: logoClass, controls: controlsClass } = styles;
 
-const Nav = () => {
+const Nav: FC = () => {
+  const router = useRouter();
+  const isOpenSettings = router.asPath.includes('settings');
+  const handleToggleSettings = () => {
+    if (isOpenSettings) router.push('/');
+    else router.push('/settings');
+  };
+
   return (
     <div className={navClass}>
       <div className={logoClass}>Token Charts</div>
       <div className={controlsClass}>
-        <button onClick={() => {}}>Update</button>
-        <button onClick={() => {}}>Settings</button>
+        <button>Update</button>
+        <button onClick={handleToggleSettings}>{isOpenSettings ? 'Close' : 'Settings'}</button>
       </div>
     </div>
   );
